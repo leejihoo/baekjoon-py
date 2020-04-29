d727c8ffc51f50e956309e52048ca266ed6fb316
mint97 <ajtwlsdl33@gmail.com>
2020-04-29 15:58:16 +0900

    백준 2741 N 찍기

--- 10816.py
+++ 10816.py
@@ -1,23 +1,31 @@
-N = input()
-N = int(N)
-li = list(map(int, input().split()))
-M = input()
-M = int(M)
-li2 = list(map(int, input().split()))
+def binary_search(key, list1):
+    len1 = len(list1)
+    list1 = list(list1)
+    low = 0
+    high = len1-1
+    while low < high:
+        mid = (low+high)//2
+        if key > list1[mid]:
+            low = mid + 1
+        elif key < list1[mid]:
+            high = mid - 1
+        else:
+            return mid
+    return -1
 
-li3 = []
-for i in range(M):
-    temp = []
-    temp.append(li2[i])
-    temp.append(0)
-    li3.append(temp)
-
-di = dict(li3)
-for i in li:
-    if i in di:
-        di[i] = di[i] + 1
-for val in di.values():
-    print(val, end=' ')
-
-
+if __name__ == "__main__":
+    N = input()
+    N = int(N)
+    li1 = list(map(int, input().split()))
+    set1 = set(li1)
+    M = input()
+    M = int(M)
+    li2 = list(map(int, input().split()))
+    di = dict()
+    li3 = [0 for i in range(len(set1))]
+    for i in li1:
+        if binary_search(i, set1) > 0:
+            li3[binary_search(i, set1)] = li3[binary_search(i, set1)] + 1
+    for i in li2:
+        print(li3[binary_search(i, set1)], end='')
 
---
+++ 2741.py
@@ -0,0 +1,4 @@
+N = input()
+N = int(N)
+for i in range(N):
+    print(i+1)
