dd4cc302c5368fc64df81f94eb141efa3909c817
mint97 <ajtwlsdl33@gmail.com>
2020-05-05 21:31:21 +0900

    백준 10828 스택 - sys.stdin.readline을 사용하여 시간복잡도를 작게만듬

---
+++ 10828.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+N = input().strip()
+N = int(N)
+stack = list()
+for i in range(N):
+    li = input().split()
+    if li[0] == "push":
+        StLen = len(stack)
+        StLen = int(StLen)
+        stack.append(li[1])
+    elif li[0] == "pop":
+        StLen = len(stack)
+        StLen = int(StLen)
+        if StLen > 0:
+            print(stack[StLen - 1])
+            del stack[StLen - 1]
+        else:
+            print("-1")
+    elif li[0] == "size":
+        StLen = len(stack)
+        StLen = int(StLen)
+        print(StLen)
+    elif li[0] == "empty":
+        StLen = len(stack)
+        StLen = int(StLen)
+        if StLen > 0:
+            print("0")
+        else:
+            print("1")
+    elif li[0] == "top":
+        StLen = len(stack)
+        StLen = int(StLen)
+        if StLen > 0:
+            print(stack[StLen - 1])
+        else:
+            print("-1")
+
