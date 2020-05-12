735a88421ee3068f101aeb2417eb5ebdf80a912c
mint97 <ajtwlsdl33@gmail.com>
2020-05-12 17:02:32 +0900

    백준 11723 집합 - 배열을 비트마스크처럼 사용 / readline으로 시간복잡도를 줄임

--- 11723.py
+++ 11723.py
@@ -1,85 +1,59 @@
-# import sys
-# input1 = sys.stdin.readline
-#
-#
-# # def binary_search(li, val):
-# # #     low = 0
-# # #     high = len(li) - 1
-# # #     while low <= high:
-# # #         mid = (low + high) // 2
-# # #         if li[mid] < val:
-# # #             low = mid + 1
-# # #         elif li[mid] > val:
-# # #             high = mid - 1
-# # #         else:
-# # #             return True
-# # #     return False
-#
-#
-# N = input()
-# N = int(N)
-# li1 = []
-# for i in range(N):
-#     word = input1().split()
-#     if word[0] == "add":
-#         if word[1] not in li1:
-#         # if not binary_search(li1, word[1]):
-#             li1.append(word[1])
-#     elif word[0] == "remove":
-#         if word[1] in li1:
-#         # if binary_search(li1, word[1]):
-#             li1.remove(word[1])
-#     elif word[0] == "check":
-#         if word[1] in li1:
-#         # if binary_search(li1, word[1]):
-#             print("1")
-#         else:
-#             print("0")
-#     elif word[0] == "toggle":
-#         if word[1] in li1:
-#         # if binary_search(li1, word[1]):
-#             li1.remove(word[1])
-#         else:
-#             li1.append(word[1])
-#     elif word[0] == "all":
-#         li1.clear()
-#         # while len(li1) > 0:
-#         #     del li1[0]
-#         for j in range(20):
-#             li1.append(str(j+1))
-#     elif word[0] == "empty":
-#         li1.clear()
-#         # while len(li1) > 0:
-#         #     del li1[0]
 import sys
+input1 = sys.stdin.readline
 
-resultSet = 0
-executeCount = sys.stdin.readline()
 
-for i in range(0, int(executeCount)):
-    operatorStr = sys.stdin.readline()
-    executeOperator = operatorStr.split(" ")[0]
-    executeNumber = 0
+# def binary_search(li, val):
+# #     low = 0
+# #     high = len(li) - 1
+# #     while low <= high:
+# #         mid = (low + high) // 2
+# #         if li[mid] < val:
+# #             low = mid + 1
+# #         elif li[mid] > val:
+# #             high = mid - 1
+# #         else:
+# #             return True
+# #     return False
 
-    if executeOperator != "all\n" and executeOperator != "empty\n":
-        executeNumber = int(operatorStr.split(" ")[1]) - 1
 
-    if executeOperator == "add":
-        resultSet |= (1 << executeNumber)
-    elif executeOperator == "remove":
-        resultSet &= ~(1 << executeNumber)
-    elif executeOperator == "check":
-        if resultSet & (1 << executeNumber):
+N = input()
+N = int(N)
+li1 = [False for i in range(21)]
+for i in range(N):
+    word = input1().split()
+    if word[0] == "add":
+        # if word[1] not in li1:
+        # if not binary_search(li1, word[1]):
+        #     li1.append(word[1])
+            word1 = int(word[1])
+            li1[word1] = True
+    elif word[0] == "remove":
+        # if word[1] in li1:
+        # if binary_search(li1, word[1]):
+        #     li1.remove(word[1])
+            word1 = int(word[1])
+            li1[word1] = False
+    elif word[0] == "check":
+        word1 = int(word[1])
+        if li1[word1] == True:
+        # if binary_search(li1, word[1]):
             print("1")
         else:
             print("0")
-    elif executeOperator == "toggle":
-        if resultSet & (1 << executeNumber):
-            resultSet = (resultSet & ~(1 << executeNumber))
-        else:
-            resultSet |= (1 << executeNumber)
-    elif executeOperator == "all\n":
-        resultSet = (1 << 20) - 1
-    elif executeOperator == "empty\n":
-        resultSet &= 0
-
+    elif word[0] == "toggle":
+        word1 = int(word[1])
+        li1[word1] = not li1[word1]
+        # if binary_search(li1, word[1]):
+        #     li1[word1] = False
+        # else:
+        #     li1[word1] = True
+    elif word[0] == "all":
+        # while len(li1) > 0:
+        #     del li1[0]
+        for j in range(21):
+            li1[j] = True
+    elif word[0] == "empty":
+        for j in range(21):
+            li1[j] = False
+        # while len(li1) > 0:
+        #     del li1[0]
